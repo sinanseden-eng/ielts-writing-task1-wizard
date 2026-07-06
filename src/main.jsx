331403f8ba1279d98a5000fe1c1e237f61a83f89
sinanseden-eng <sinanseden@gmail.com>
2026-07-06 03:47:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
