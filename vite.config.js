9cdd3d578c100ccaea787f7c0b783691b83059da
sinanseden-eng <sinanseden@gmail.com>
2026-07-06 03:17:14 +0300

    Configure server port and build options in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
     open: true
-     rollupOptions: {
+  },
+
+  build: {
+    rollupOptions: {
       external: ['fsevents']
+    }
   }
 })
