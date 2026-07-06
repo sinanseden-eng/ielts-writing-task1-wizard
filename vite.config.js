00023f459588f7430f9ecc24968335056e3fc4d1
sinanseden-eng <sinanseden@gmail.com>
2026-07-06 03:48:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      // 1. Mark Node.js built-ins as external so they are ignored by the browser bundler
-      external: ['fs', 'path', 'stream', 'os', 'url', 'https', 'rollup'],
-      
-      // 2. Explicitly tell Rollup to only look at index.html
-      // This stops it from auto-scanning your netlify functions folder
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  // Ensure the build only processes the 'src' folder
-  root: './',
-  publicDir: 'public'
 });
